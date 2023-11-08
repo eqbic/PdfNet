5d0e338e49cc4f0afe96cb01dd905ec4fb737529
Erich Querner <querner@interactive-scape.com>
2023-11-08 09:44:27 +0100

    add basic math operations to int vector

--- PdfNet.Core/Vector2Int.cs
+++ PdfNet.Core/Vector2Int.cs
@@ -10,5 +10,16 @@
             X = x;
             Y = y;
         }
+
+        public static Vector2Int operator +(Vector2Int a, Vector2Int b) => new Vector2Int(a.X + b.X, a.Y + b.Y);
+        public static Vector2Int operator -(Vector2Int a) => new Vector2Int(-a.X, -a.Y);
+        public static Vector2Int operator -(Vector2Int a, Vector2Int b) => new Vector2Int(a.X - b.X, a.Y - b.Y);
+        public static Vector2Int operator *(Vector2Int a, int s) => new Vector2Int(a.X * s, a.Y * s);
+        public static Vector2Int operator *(Vector2Int a, float s) => new Vector2Int((int)(a.X * s), (int)(a.Y * s));
+
+        public override string ToString()
+        {
+            return $"X: {X}, Y: {Y}";
+        }
     }
 }
