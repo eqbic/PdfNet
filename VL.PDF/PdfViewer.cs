2fa4a9727682cad205783c0b787608bc63480f8b
Erich Querner <querner@interactive-scape.com>
2023-11-08 09:46:58 +0100

    moved vl project

---
+++ VL.PDF/PdfViewer.cs
@@ -0,0 +1,34 @@
+﻿using PdfNet.Core;
+using SkiaSharp;
+using Stride.Core.Mathematics;
+
+namespace VL.PDF;
+
+public static class PdfViewer
+{
+    public static PdfDocument LoadDocument(string path, string password = "")
+    {
+        return new PdfDocument(path, password);
+    }
+
+    public static void UpdatePageSizes(PdfDocument document, PdfViewport viewport)
+    {
+        document.UpdatePageSizes(viewport);
+    }
+
+    public static SKImage RenderDocument(PdfDocument document, PdfViewport viewport, SkiaTexture texture)
+    {
+        document.Render(viewport, texture);
+        return texture.Image;
+    }
+        
+    public static System.Numerics.Vector2 ToVec2 (Vector2 vector)
+    {
+        return new System.Numerics.Vector2(vector.X, vector.Y);
+    }
+    
+    public static System.Numerics.Vector2 ToVec2 (Int2 vector)
+    {
+        return new System.Numerics.Vector2(vector.X, vector.Y);
+    }
+}
---
+++ VL.PDF/SkiaTexture.cs
@@ -0,0 +1,28 @@
+﻿using PdfNet.Core;
+using SkiaSharp;
+using Stride.Core.Mathematics;
+
+namespace VL.PDF;
+
+public class SkiaTexture : PdfTexture
+{
+    private SKImageInfo _info;
+
+    public Int2 Resolution
+    {
+        get => new Int2(base.Resolution.X, base.Resolution.Y);
+        set
+        {
+            base.Resolution = new Vector2Int(value.X, value.Y);
+            _info.Width = Resolution.X;
+            _info.Height = Resolution.Y;
+        }
+    }
+    
+    public SKImage Image => SKImage.FromPixels(_info, DataPointer);
+    
+    public SkiaTexture(Int2 resolution) : base(new Vector2Int(resolution.X, resolution.Y))
+    {
+        _info = new SKImageInfo(resolution.X, resolution.Y, SKColorType.Bgra8888);
+    }
+}
