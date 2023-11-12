7e1096fa6095c8d2b1f49d8c9f0f138484cb0ff5
Erich Querner <querner@interactive-scape.com>
2023-11-12 13:21:19 +0100

    add extension method to get size of rectangle as vector2

---
+++ PdfNet/Core/ExtensionUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace PdfNet.Core;
+
+public static class ExtensionUtils
+{
+    public static Vector2 Size(this RectangleF rectangle)
+    {
+        return new Vector2(rectangle.Size.Width, rectangle.Size.Height);
+    }
+}
