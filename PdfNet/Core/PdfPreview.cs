32a38a24733a78ef48df8353a1d2288603a8ce8c
Erich Querner <querner@interactive-scape.com>
2023-11-12 15:27:45 +0100

    add class to render preview of the whole document

---
+++ PdfNet/Core/PdfPreview.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace PdfNet.Core;
+
+public class PdfPreview
+{
+    private PdfViewport _viewport;
+    private PdfDocument _document;
+    private PdfTexture _texture;
+    
+    public Vector2 Resolution { get; }
+    
+    public PdfPreview(PdfDocument document, float scale)
+    {
+        _document = document;
+        Resolution = _document.DocumentSize * scale;
+        _viewport = new PdfViewport(Resolution);
+        _texture = new PdfTexture(Resolution);
+    }
+
+    public PdfTexture Render()
+    {
+        for(var i = 0; i < _document.PageCount; i++)
+        {
+            var page = _document.GetPage(i);
+            page.UpdatePageSize(_viewport);
+            page.Render(_viewport, _texture);
+        }
+
+        return _texture;
+    }
+}
